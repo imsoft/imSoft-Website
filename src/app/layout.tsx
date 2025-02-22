7a6d6c75cea52c6ba6be18ac94b3b0d43cdac0f2
Brandon Uriel García Ramos <weareimsoft@gmail.com>
2025-02-22 14:15:27 -0600

    Se agrego Vercel SpeedInsigths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { FooterSection } from "@/components/Footer";
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body className={`${montserrat.variable} antialiased`}>
         {children}
         <FooterSection />
-        <Analytics/>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
