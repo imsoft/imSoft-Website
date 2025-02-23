8efa13207d8da66542000f5693c14bd0f8d61544
Brandon Uriel García Ramos <weareimsoft@gmail.com>
2025-02-22 18:36:02 -0600

    Se hizo una modificacion en la seccion del Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { ServicesSection } from "@/components/services/Services";
 const Home = () => {
   return (
     <>
-      <section className="min-h-screen flex flex-col sm:flex-row items-center justify-center py-16 sm:py-24 lg:py-32">
+      <section className="min-h-screen flex flex-col sm:flex-row items-center justify-center pb-8 sm:pb-12 lg:pb-16">
         <HeroSection />
       </section>
       <section className="min-h-screen flex flex-col sm:flex-row items-center justify-center py-16 sm:py-24 lg:py-32">
