3ff80614cb8e5d2e00453a5f2de90494c138a6bd
Brandon Uriel García Ramos <weareimsoft@gmail.com>
2025-03-02 16:48:45 -0600

    Cambio de tamaño en las imagenes en articulos del blog

--- src/components/blog/Post.tsx
+++ src/components/blog/Post.tsx
@@ -10,15 +10,18 @@ const builder = imageUrlBuilder(client);
 
 export const Post = ({ post }: { post: SanityDocument }) => {
   return (
-    <main className="container mx-auto prose prose-xl px-4 py-24" aria-label={`Publicación del blog: ${post.title}`}>
+    <main
+      className="container mx-auto prose prose-xl px-4 py-24"
+      aria-label={`Publicación del blog: ${post.title}`}
+    >
       <h1 aria-label="Título del artículo">{post.title}</h1>
       <p>{post.description}</p>
       {post?.mainImage ? (
         <Image
-          src={builder.image(post.mainImage).width(300).height(300).url()}
+          src={builder.image(post.mainImage).width(1280).height(720).url()}
           alt={post.mainImage.alt}
-          width={300}
-          height={300}
+          width={1280}
+          height={720}
         />
       ) : null}
       {post?.body ? <PortableText value={post.body} /> : null}
