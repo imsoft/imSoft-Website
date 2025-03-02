ebd6db38adc65427e0ed4fb6d3b5291530a1288a
Brandon Uriel García Ramos <weareimsoft@gmail.com>
2025-03-02 16:51:34 -0600

    Se agrego la etiqueta article a los articulos del blog

--- src/components/blog/Post.tsx
+++ src/components/blog/Post.tsx
@@ -10,21 +10,23 @@ const builder = imageUrlBuilder(client);
 
 export const Post = ({ post }: { post: SanityDocument }) => {
   return (
-    <main
-      className="container mx-auto prose prose-xl px-4 py-24"
-      aria-label={`Publicación del blog: ${post.title}`}
-    >
-      <h1 aria-label="Título del artículo">{post.title}</h1>
-      <p>{post.description}</p>
-      {post?.mainImage ? (
-        <Image
-          src={builder.image(post.mainImage).width(1280).height(720).url()}
-          alt={post.mainImage.alt}
-          width={1280}
-          height={720}
-        />
-      ) : null}
-      {post?.body ? <PortableText value={post.body} /> : null}
-    </main>
+    <article>
+      <main
+        className="container mx-auto prose prose-xl px-4 py-24"
+        aria-label={`Publicación del blog: ${post.title}`}
+      >
+        <h1 aria-label="Título del artículo">{post.title}</h1>
+        <p>{post.description}</p>
+        {post?.mainImage ? (
+          <Image
+            src={builder.image(post.mainImage).width(1280).height(720).url()}
+            alt={post.mainImage.alt}
+            width={1280}
+            height={720}
+          />
+        ) : null}
+        {post?.body ? <PortableText value={post.body} /> : null}
+      </main>
+    </article>
   );
 };
