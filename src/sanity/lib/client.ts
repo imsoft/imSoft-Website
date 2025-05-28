9c9758ac099f52930f896b0e6b48f5fb7d959a1f
Brandon Uriel García Ramos <weareimsoft@gmail.com>
2025-05-28 13:25:48 -0600

    Actualización de configuración del cliente Sanity

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,5 +6,7 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  // useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // para obtener datos siempre frescos en SSR/ISR
+  token: process.env.SANITY_API_READ_TOKEN, // asegúrate de tenerlo en Vercel
 })
